8d29928e5b6d5cc77f0ede82669fb1535dcf0a8b
Himadri Chakrabarty <107918769+HimadriChakra12@users.noreply.github.com>
2025-03-30 13:59:16 -0700

    added choco config

---
+++ choco/choco.ps1
@@ -0,0 +1,20 @@
+# Installation
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex
+
+# Optimization
+choco feature enable --name=allowGlobalConfirmation
+choco feature enable --name=useRememberedArgumentsForUpgrades
+choco feature enable --name=usePackageExitCodes
+choco feature enable --name=useEnhancedExitCodes
+choco feature enable --name=showDownloadProgress
+choco feature enable --name=useFipsCompliantChecksums
+choco feature enable --name=logCommandOutput
+choco config set cacheLocation C:\choco-cache
+choco config set commandExecutionTimeoutSeconds 9999
+choco config set webRequestTimeoutSeconds 999
+choco config set virusCheck false
+
+# Execution
+choco feature enable --name=usePackageExitCodes
+choco feature enable --name=useEnhancedExitCodes
+
