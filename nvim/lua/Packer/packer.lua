6f3ed7f82a323ed758d286847969fbba71c675aa
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2025-05-13 08:29:09 +0000

    a

--- nvim/lua/Packer/packer.lua
+++ nvim/lua/Packer/packer.lua
@@ -7,39 +7,6 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
     
-    use {
-	    'vzze/cmdline.nvim',
-	    config = function()
-		    require('cmdline').setup({
-			    cmdtype = ":/", -- you can also add / and ? by using ":/?"
-			    -- as a string
-
-			    window = {
-				    matchFuzzy = true,
-				    offset     = 1,    -- depending on 'cmdheight' you might need to offset
-				    debounceMs = 10    -- the lower the number the more responsive however
-				    -- more resource intensive
-			    },
-
-			    hl = {
-				    default   = "Pmenu",
-				    selection = "PmenuSel",
-				    directory = "Directory",
-				    substr    = "LineNr"
-			    },
-
-			    column = {
-				    maxNumber = 6,
-				    minWidth  = 20
-			    },
-
-			    binds = {
-				    next = "<Tab>",
-				    back = "<S-Tab>"
-			    }
-		    })
-	    end
-    }
   
     use 'folke/flash.nvim'
 
